74f20c074648fe45ba56077074feb78f552de855
Slomako <lomakosv@gmail.com>
2023-05-01 23:11:03 +0300

    added step, allure

--- src/test/java/ru/lomakosv/tests/ReqresInTests.java
+++ src/test/java/ru/lomakosv/tests/ReqresInTests.java
@@ -1,109 +1,131 @@
 package ru.lomakosv.tests;
 
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import ru.lomakosv.api.model.*;
 
 import java.time.Clock;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.*;
-import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.hasItems;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static ru.lomakosv.tests.Specification.request;
+import static ru.lomakosv.tests.Specification.requestSpec;
+import static ru.lomakosv.tests.Specification.responseSpec;
 
+@Tag("api_test")
+@DisplayName("api тесты на сайт Reqres.in")
 public class ReqresInTests {
 
+    @DisplayName("email у users/2")
+    @Owner("SLomako")
     @Test
     void checkSingleUserDataEmail() {
         String expendDataEmail = "janet.weaver@reqres.in";
 
-        String actualDataEmail = given().spec(request).
+
+        String actualDataEmail = step("Запрос", () ->
+        given(requestSpec).
         when()
                 .get("/users/2").
         then()
-                .log().all()
-                .statusCode(200).extract().path("data.email");
-
+                .spec(responseSpec)
+                .statusCode(200).extract().path("data.email"));
+        step("Проверка", () -> {
         Assertions.assertEquals(expendDataEmail, actualDataEmail);
+        });
     }
 
+    @DisplayName("соотвествие возвращаемых name и job отправленным")
+    @Owner("SLomako")
     @Test
     void successfulCreateUser() {
         String name = "morpheus";
         String job = "leader";
         Users users = new Users("morpheus", "leader");
-        CreateUser createUser = given().spec(request)
-                .contentType(JSON)
+        CreateUser createUser = step("Запрос", () ->
+        given(requestSpec)
                 .body(users).
         when()
                 .post("/users").
-        then().
-                log().all().
-                statusCode(201).
-                extract().
-                as(CreateUser.class);
+        then()
+                .spec(responseSpec)
+                .statusCode(201)
+                .extract()
+                .as(CreateUser.class));
 
         assertNotNull(createUser.getId());
         assertNotNull(createUser.getCreatedAt());
-        Assertions.assertEquals(name, createUser.getName());
-        Assertions.assertEquals(job, createUser.getJob());
+        step("Проверка", () -> {
+        Assertions.assertEquals(name, createUser.getName());});
+            step("Проверка", () -> {
+        Assertions.assertEquals(job, createUser.getJob());});
     }
 
+    @DisplayName("сообщения и статус-кода 400 при ошибки регистрации")
+    @Owner("SLomako")
     @Test
     void usSuccessfulCreateUser() {
         String error = "Missing password";
         Register register = new Register("sydney@fife");
-        UnSuccessfulRegister unSuccessfulRegister = given().spec(request)
-                .contentType(JSON)
+        UnSuccessfulRegister unSuccessfulRegister = step("Запрос", () ->
+        given(requestSpec)
                 .body(register).
         when()
                 .post("/register").
         then()
+                .spec(responseSpec)
                 .statusCode(400)
                 .extract()
-                .as(UnSuccessfulRegister.class);
-
-        Assertions.assertEquals(error, unSuccessfulRegister.getError());
+                .as(UnSuccessfulRegister.class));
+        step("Проверка", () -> {
+        Assertions.assertEquals(error, unSuccessfulRegister.getError());});
     }
 
+    @DisplayName("статус кода 204 при запросе на удаление пользователя")
+    @Owner("SLomako")
     @Test
     void deleteUserTest() {
-        given().spec(request).
-        when().
-                delete("/users/2").
-        then().
-                statusCode(204);
+        step("Запрос");
+        given(requestSpec).
+        when()
+                .delete("/users/2").
+        then()
+                .spec(responseSpec)
+                .statusCode(204);
     }
 
+    @DisplayName("одинаковость(до минут) системного времени у ново созданного пользователя и системы")
+    @Owner("SLomako")
     @Test
     void timeTest() {
         Users users = new Users("morpheus", "zion resident");
-        CreateUserTime createUserTime = given().spec(request).
-                contentType(JSON).
+        CreateUserTime createUserTime = step("Запрос", () ->
+        given(requestSpec).
                 body(users).
         when()
                 .put("/users/2").
         then()
+                .spec(responseSpec)
                 .statusCode(200)
                 .extract()
-                .as(CreateUserTime.class);
+                .as(CreateUserTime.class));
 
-        String regexUserTime = "(.{8})$";
-        String regexSystemTime = "(.{14})$";
-        
-        String currentTime = Clock.systemUTC().instant().toString().replaceAll(regexSystemTime, "");
-        Assertions.assertEquals(currentTime, createUserTime.getUpdatedAt().replaceAll(regexUserTime, ""));
-
-        System.out.println(currentTime);
-        System.out.println(createUserTime.getUpdatedAt().replaceAll(regexUserTime, ""));
-
+        String regex = "(.{3}\\..*)$";
+        String currentTime = Clock.systemUTC().instant().toString().replaceAll(regex, "");
+        step("Проверка", () -> {
+        Assertions.assertEquals(currentTime, createUserTime.getUpdatedAt().replaceAll(regex, ""));});
     }
 
+    @DisplayName("имя после сортировки ответа по году")
+    @Owner("SLomako")
     @Test
     void checkListResource() {
-        given().
-                spec(request).
+        step("Запрос");
+        given(requestSpec).
         when()
                 .get("/unknown").
         then()
--- src/test/java/ru/lomakosv/tests/Specification.java
+++ src/test/java/ru/lomakosv/tests/Specification.java
@@ -1,18 +1,28 @@
 package ru.lomakosv.tests;
 
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
+import static ru.lomakosv.helpers.CustomApiListener.withCustomTemplates;
 
 public class Specification {
 
-    public static RequestSpecification request =
+    public static RequestSpecification requestSpec =
             with()
                     .baseUri("https://reqres.in")
                     .basePath("/api")
-                    .log()
-                    .all()
+                    .log().all()
+                    .filter(withCustomTemplates())
                     .contentType(JSON);
+
+    public static ResponseSpecification responseSpec = new ResponseSpecBuilder().
+            log(STATUS).
+            log(BODY).
+            build();
 
 }
